dc623ba964b24f33bd0c62ca3b67793a688c93d9
nitin <nitinbhusale91@gmail.com>
2022-02-21 11:33:15 +0530

    added second print statement

--- index.js
+++ index.js
@@ -5,5 +5,8 @@ public class A
 
 public static void main(String args[])
 {
-System.out.println("Hello World");}
+System.out.println("Hello World");
+System.out.println("secondprintstatement);
+
+}
 }
